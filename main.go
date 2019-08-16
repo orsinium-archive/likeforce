956d1f0769672151010e5852434409bf93b25783
Gram <master_fess@mail.ru>
2019-08-16 13:30:47 +0200

    +main

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"os"
+
+	"github.com/francoispqt/onelog"
+	"github.com/orsinium/likeforce/likeforce"
+)
+
 func main() {
-	config = likeforce.ReadConfig("config.toml")
+	logger := onelog.New(os.Stdout, onelog.ALL)
+	config, err := likeforce.ReadConfig("config.toml")
+	if err != nil {
+		logger.FatalWith("cannot read config").Err("error", err).Write()
+		return
+	}
+	likes, err := likeforce.NewLikes(config.Redis)
+	if err != nil {
+		logger.FatalWith("cannot create Redis connection").Err("error", err).Write()
+		return
+	}
+	tg, err := likeforce.NewTelegram(config.Telegram, likes)
+	if err != nil {
+		logger.FatalWith("cannot create Telegram connection").Err("error", err).Write()
+		return
+	}
+	logger.Info("Serve")
+	tg.Serve()
 }
